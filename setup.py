1cb346273357584adb34bf0639a2af1e0118944a
Yolley <comingreal@gmail.com>
2023-02-16 00:27:35 +0900

    upgrade version to alpha

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ requirements = [
 
 setup(
     name="instagrapi",
-    version="1.16.40",
+    version="1.17.0a.1",
     author="Mikhail Andreev",
     author_email="x11org@gmail.com",
     license="MIT",
